6b78643e50170bf9d5ad4010df6a57d6d277cb8e
rejan chhantyal <rejanchhantyal122@gmail.com>
2024-09-19 20:19:35 +0545

    updated by Rojip Chhantyal on september 19, 8:19PM

--- matrix subtraction.cpp
+++ matrix subtraction.cpp
@@ -1,33 +1,35 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main(){
 	int size;
-	printf("Enter a size of the matrix : ");
+	
+	printf("Enter a size of matrix : ");
 	scanf("%d",&size);
 	
-	int first_matrix[size][size],second_matrix[size][size],result[size][size];
+	int first_matrix[size][size],second_matrix[size][size],result_matrix[size][size];
 	
-	printf("Enter a element of first matrix : \n");
+	printf("Enter a first matrix elements : \n");
 	for(int i=0;i<size;i++){
 		for(int j=0;j<size;j++){
 			scanf("%d",&first_matrix[i][j]);
 		}
 	}
-	printf("Enter a element of second matrix : \n");
+	
+	printf("Enter a second matrix elements : \n");
 	for(int i=0;i<size;i++){
 		for(int j=0;j<size;j++){
 			scanf("%d",&second_matrix[i][j]);
 		}
 	}
+	
 	for(int i=0;i<size;i++){
 		for(int j=0;j<size;j++){
-			result[i][j]=first_matrix[i][j] + second_matrix[i][j];
+			result_matrix[i][j]= first_matrix[i][j] - second_matrix[i][j];
 		}
 	}
-	printf("The sum of two given matrix is :\n");
+	printf("The subtraction of two given matrix is : \n");
 	for(int i=0;i<size;i++){
 		for(int j=0;j<size;j++){
-			printf("%d ",result[i][j]);
+			printf("%d ",result_matrix[i][j]);
 		}
 		printf("\n");
 	}
